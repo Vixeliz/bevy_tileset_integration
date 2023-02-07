49e3faa53c5fbc91b0d2b17cb961f26fc79492ee
Mylah Dee <vixeliz@icloud.com>
2023-02-07 13:43:10 -0600

    Change to my crappy sprites and clean up example

--- examples/tileset.rs
+++ examples/tileset.rs
@@ -13,7 +13,7 @@ struct MyTileset {
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugin(TilemapPlugin)
         .add_plugin(TilesetPlugin::default())
         .add_plugin(TilesetIntePlugin)
@@ -23,7 +23,7 @@ fn main() {
         .add_system(test_chunk)
         .add_system(move_camera)
         .add_system(my_cursor_system)
-        .add_system(random_tiles)
+        .add_system(tile_set)
         .run();
 }
 
@@ -42,7 +42,9 @@ fn test_chunk(
     }
 
     if let Some(tileset) = tilesets.get_by_name("My Awesome Tileset") {
-        commands.spawn((Camera2dBundle::default(), CameraTag));
+        let mut camera = Camera2dBundle::default();
+        camera.projection.scale = 0.5;
+        commands.spawn((camera, CameraTag));
         let chunk = Chunk::new(IVec2 { x: 0, y: 0 }, 1, Some("Grass".to_string()));
         chunk_manager.add_chunk("My Awesome Tileset".to_string(), chunk);
         chunk_manager.add_new_chunk(IVec2 { x: -1, y: 0 }, "My Awesome Tileset".to_string(), 1);
@@ -60,6 +62,7 @@ fn load_tileset(mut my_tileset: ResMut<MyTileset>, asset_server: Res<AssetServer
 #[derive(Resource, Default)]
 struct WorldMousePos(Vec2);
 
+// Converts cursor screen pos to world pos. Not my code found from bevy cheatbook
 fn my_cursor_system(
     // need to get window dimensions
     wnds: Res<Windows>,
@@ -120,24 +123,22 @@ fn move_camera(
     }
 }
 
-fn random_tiles(
+fn tile_set(
     mut chunk_manager: ChunkManager,
     chunk_query: Query<&Chunk>,
     world_mouse: Res<WorldMousePos>,
+    buttons: Res<Input<MouseButton>>,
 ) {
-    for chunk in chunk_query.iter() {
-        let key = chunk.pos;
+    if buttons.just_pressed(MouseButton::Left) {
         let (chunk_pos, tile_pos) =
             chunk_manager.chunk_pos_from_world(world_mouse.0, 1, "My Awesome Tileset".to_string());
-        if chunk.pos == chunk_pos {
-            chunk_manager.set_tile_in_chunk(
-                tile_pos,
-                key,
-                "My Awesome Tileset".to_string(),
-                "Dirt".to_string(),
-                chunk.layer,
-            );
-        }
+        chunk_manager.set_tile_in_chunk(
+            tile_pos,
+            chunk_pos,
+            "My Awesome Tileset".to_string(),
+            "Dirt".to_string(),
+            1,
+        );
     }
     //= How you could delete a chunk =//
     //chunk_manager.remove_chunk(key, 0);
