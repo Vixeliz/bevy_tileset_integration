cb0beffebecce3ccd8e03a7669a3bf509a79a66e
Mylah Dee <vixeliz@icloud.com>
2023-02-06 22:09:51 -0600

    Add getting tile and chunk from world_pos

--- examples/tileset.rs
+++ examples/tileset.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::RenderTarget};
 use bevy_ecs_tilemap::prelude::*;
 use bevy_tileset::prelude::*;
 
@@ -18,11 +18,16 @@ fn main() {
         .add_plugin(TilesetPlugin::default())
         .add_plugin(TilesetIntePlugin)
         .init_resource::<MyTileset>()
+        .init_resource::<WorldMousePos>()
         .add_startup_system(load_tileset)
         .add_system(test_chunk)
+        .add_system(my_cursor_system)
         .add_system(random_tiles)
         .run();
 }
+
+#[derive(Component)]
+pub struct CameraTag;
 
 fn test_chunk(
     mut commands: Commands,
@@ -36,10 +41,12 @@ fn test_chunk(
     }
 
     if let Some(tileset) = tilesets.get_by_name("My Awesome Tileset") {
-        commands.spawn(Camera2dBundle::default());
+        commands.spawn((Camera2dBundle::default(), CameraTag));
         let chunk = Chunk::new(IVec2 { x: 0, y: 0 }, 1, Some("Grass".to_string()));
         chunk_manager.add_chunk("My Awesome Tileset".to_string(), chunk);
-        chunk_manager.add_new_chunk(IVec2 { x: 0, y: 0 }, "My Awesome Tileset".to_string(), 2);
+        chunk_manager.add_new_chunk(IVec2 { x: -1, y: 0 }, "My Awesome Tileset".to_string(), 1);
+        chunk_manager.add_new_chunk(IVec2 { x: -1, y: -1 }, "My Awesome Tileset".to_string(), 1);
+        chunk_manager.add_new_chunk(IVec2 { x: 0, y: -1 }, "My Awesome Tileset".to_string(), 1);
         *has_ran = true;
     }
 }
@@ -49,29 +56,67 @@ fn load_tileset(mut my_tileset: ResMut<MyTileset>, asset_server: Res<AssetServer
     my_tileset.handle = Some(asset_server.load("tilesets/my_tileset.ron"));
 }
 
-fn random_tiles(mut chunk_manager: ChunkManager, chunk_query: Query<&Chunk>) {
+#[derive(Resource, Default)]
+struct WorldMousePos(Vec2);
+
+fn my_cursor_system(
+    // need to get window dimensions
+    wnds: Res<Windows>,
+    // query to get camera transform
+    q_camera: Query<(&Camera, &GlobalTransform), With<CameraTag>>,
+    mut world_mouse: ResMut<WorldMousePos>,
+) {
+    // get the camera info and transform
+    // assuming there is exactly one main camera entity, so query::single() is OK
+    if let Ok((camera, camera_transform)) = q_camera.get_single() {
+        // get the window that the camera is displaying to (or the primary window)
+        let wnd = if let RenderTarget::Window(id) = camera.target {
+            wnds.get(id).unwrap()
+        } else {
+            wnds.get_primary().unwrap()
+        };
+
+        // check if the cursor is inside the window and get its position
+        if let Some(screen_pos) = wnd.cursor_position() {
+            // get the size of the window
+            let window_size = Vec2::new(wnd.width() as f32, wnd.height() as f32);
+
+            // convert screen position [0..resolution] to ndc [-1..1] (gpu coordinates)
+            let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
+
+            // matrix for undoing the projection and camera transform
+            let ndc_to_world =
+                camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+
+            // use it to convert ndc to world-space coordinates
+            let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
+
+            // reduce it to a 2D value
+            let world_pos: Vec2 = world_pos.truncate();
+            world_mouse.0 = world_pos;
+        }
+    }
+}
+
+fn random_tiles(
+    mut chunk_manager: ChunkManager,
+    chunk_query: Query<&Chunk>,
+    world_mouse: Res<WorldMousePos>,
+) {
     for chunk in chunk_query.iter() {
-        if chunk.layer != 1 {
-            let key = chunk.pos;
-            let block_type = match rand::thread_rng().gen_range(0..4) {
-                0 => "Grass".to_string(),
-                1 => "Dirt".to_string(),
-                2 => "Glass".to_string(),
-                _ => "Wall".to_string(),
-            };
-            let tile_pos = UVec2::new(
-                rand::thread_rng().gen_range(0..CHUNK_SIZE) as u32,
-                rand::thread_rng().gen_range(0..CHUNK_SIZE) as u32,
-            );
+        let key = chunk.pos;
+        let (chunk_pos, tile_pos) =
+            chunk_manager.chunk_pos_from_world(world_mouse.0, 1, "My Awesome Tileset".to_string());
+        if chunk.pos == chunk_pos {
             chunk_manager.set_tile_in_chunk(
                 tile_pos,
                 key,
                 "My Awesome Tileset".to_string(),
-                block_type,
+                "Dirt".to_string(),
                 chunk.layer,
             );
         }
-        //= How you could delete a chunk =//
-        //chunk_manager.remove_chunk(key, 0);
     }
+    //= How you could delete a chunk =//
+    //chunk_manager.remove_chunk(key, 0);
 }
--- src/placer.rs
+++ src/placer.rs
@@ -4,7 +4,7 @@ use bevy::{ecs::system::SystemParam, prelude::*};
 use bevy_ecs_tilemap::tiles::TilePos;
 use bevy_tileset::prelude::Tilesets;
 
-use crate::prelude::{Chunk, ChunkBundle, FullUpdateChunkEvent, UpdateChunkEvent};
+use crate::prelude::{Chunk, ChunkBundle, FullUpdateChunkEvent, UpdateChunkEvent, CHUNK_SIZE};
 
 // This is the easy to use public facing api handles chunk creation,
 // Creating and managing tilemaps with ecs_tilemap etc.
@@ -23,6 +23,49 @@ pub struct ChunkManager<'w, 's> {
 
 impl<'w, 's> ChunkManager<'w, 's> {
     pub fn get_chunk(&self, pos: IVec2) {}
+    /// Returns chunk_pos and the tile position relative to that chunk. WARNING: will return 0, 0 if the world position doesn't correspond to a chunk on that layer
+    pub fn chunk_pos_from_world(
+        &self,
+        pos: Vec2,
+        layer: i32,
+        tileset_name: String,
+    ) -> (IVec2, UVec2) {
+        if let Some(current_chunk) = self.chunk_storage.chunks.get(&layer) {
+            if let Some(tileset) = self.tilesets.get_by_name(tileset_name.as_str()) {
+                let mut world_pos = (pos + tileset.tile_size() / 2.0).as_ivec2();
+                if pos.x < 0.0 {
+                    world_pos.x -= CHUNK_SIZE as i32 * tileset.tile_size().x as i32;
+                }
+                if pos.y < 0.0 {
+                    world_pos.y -= CHUNK_SIZE as i32 * tileset.tile_size().y as i32;
+                }
+                let chunk_size: IVec2 = IVec2::new(CHUNK_SIZE as i32, CHUNK_SIZE as i32);
+                let tile_size: IVec2 =
+                    IVec2::new(tileset.tile_size().x as i32, tileset.tile_size().y as i32);
+                let conv_pos = world_pos / (chunk_size * tile_size);
+                if let Some(_) = current_chunk.get(&conv_pos) {
+                    (
+                        conv_pos,
+                        UVec2::new(
+                            (world_pos.x as f32 / tileset.tile_size().x)
+                                .rem_euclid(chunk_size.x as f32) as u32,
+                            (world_pos.y as f32 / tileset.tile_size().y)
+                                .rem_euclid(chunk_size.y as f32) as u32,
+                        ),
+                    )
+                } else {
+                    // warn!("Couldn't convert position to a valid chunk");
+                    (IVec2::new(0, 0), UVec2::new(0, 0))
+                }
+            } else {
+                // warn!("Couldn't get tileset");
+                (IVec2::new(0, 0), UVec2::new(0, 0))
+            }
+        } else {
+            // warn!("Non existent layer");
+            (IVec2::new(0, 0), UVec2::new(0, 0))
+        }
+    }
     pub fn add_chunk(&mut self, tileset_name: String, chunk: Chunk) {
         if let Some(tileset) = self.tilesets.get_by_name(tileset_name.as_str()) {
             let pos = chunk.pos;
